c0328c71d9a19ead45bd9f10542a24a3f40b4f01
Ben Kooijman <ben.kooijman@protonmail.com>
2024-06-04 19:08:03 +0200

    sync code

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,5 +1,94 @@
+import re
+
+from enum import Enum
+from leafnode import LeafNode
+from parentnode import ParentNode
+from textnode import TextNode, TextTypes
+
+HEADER_REGEX = r"(?<!#)(#{1,6})\s(.*)?"
+
+class BlockTypes(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered list"
+    ORDERED_LIST =  "ordered list"
+
 def markdown_to_blocks(text):
     blocks = list()
     for block in text.lstrip("\n").rstrip("\n").split("\n\n"):
-        blocks.append(block.split("\n"))
+        blocks.append(block)
     return blocks
+
+def block_to_block_type(block):
+    lines = block.split("\n")
+
+    # Check to see if the block is a header type
+    if re.search(HEADER_REGEX, lines[0]):
+        return BlockTypes.HEADING
+    
+    # Header blocks
+    if lines[0].startswith("```") and lines[-1].startswith("```"):
+        return BlockTypes.CODE
+    # Code blocks
+    if len([x for x in lines if x.startswith(">")]) == len(lines):
+        return BlockTypes.QUOTE
+    # Unordered list blocks
+    if len([x for x in lines if x.startswith("*")]) == len(lines):
+        return BlockTypes.UNORDERED_LIST
+    # Ordered list blocks
+    if lines[0].startswith("1."):
+        line_number_index = [True] * len(lines)
+        for index, line in enumerate(lines):
+            if not line.startswith(f"{index + 1}."):
+                del line_number_index[index]
+        if len(line_number_index) == len(lines):
+            return BlockTypes.ORDERED_LIST
+    # Default to paragraph
+    return BlockTypes.PARAGRAPH
+
+def header_block_to_html(header_block):
+    try:
+        header, text = re.findall(HEADER_REGEX, header_block)[0]
+    except IndexError:
+        raise ValueError("header_block does not contain a valid markdown header")
+    
+    return LeafNode(f"h{header.count("#")}", text)
+
+def code_block_to_html(code_block):
+    lines = code_block.split("\n")
+    if not lines[0].startswith("```") or not lines[-1].startswith("```"):
+        raise ValueError("code_block is missing opening/closing characters \"```\"")
+    
+    children = list()
+    for line in lines[1:-1]:
+        children.append(TextNode(line, TextTypes.TEXT))
+
+    return ParentNode("pre", ParentNode("code", children))
+
+def quote_block_to_html(quote_block):    
+    children = list()
+    for line in quote_block.split("\n"):
+        children.append(TextNode(line, TextTypes.TEXT))
+
+    return ParentNode("pre", ParentNode("code", children))
+
+
+def unordered_list_block_to_html(unordered_list_block):
+    pass
+
+def ordered_list_block_to_html(ordered_list_block):
+    pass
+
+def markdown_to_html(markdown):
+    pass
+
+# Debugging 
+if __name__ == "__main__":
+    code_test_1 = "```python\nprint('hello world')\n```"
+    expected_code_test_1_output = ParentNode("pre", ParentNode("code", [
+        TextNode("print('hello world')", TextTypes.TEXT)
+    ]))
+    print(code_block_to_html(code_test_1))
+    print(expected_code_test_1_output)
--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -1,7 +1,9 @@
 import unittest
 
-from block_markdown import markdown_to_blocks
+from block_markdown import markdown_to_blocks, block_to_block_type, BlockTypes, header_block_to_html, code_block_to_html
 from leafnode import LeafNode
+from parentnode import ParentNode
+from textnode import TextNode, TextTypes
 
 class TestTextNode(unittest.TestCase):
 
@@ -19,8 +21,96 @@ This is the same paragraph on a new line
 * with items
 """
         expected_outcome = [
-            ['This is **bolded** paragraph'],
-            ['This is another paragraph with *italic* text and `code` here', 'This is the same paragraph on a new line'],
-            ['* This is a list', '* with items']
+            'This is **bolded** paragraph',
+            'This is another paragraph with *italic* text and `code` here\nThis is the same paragraph on a new line',
+            '* This is a list\n* with items'
         ]
         self.assertEqual(markdown_to_blocks(test_case), expected_outcome)
+
+
+    def test_block_to_block_type(self):
+        header_test_1 = "# This is a h1 header"
+        header_test_2 = "## This is a h2 header"
+        header_test_3 = "### This is a h3 header"
+        header_test_4 = "#### This is a h4 header"
+        header_test_5 = "##### This is a h5 header"
+        header_test_6 = "###### This is a h6 header"
+        header_test_7 = "######This is an invalid header"
+        header_test_8 = "####### This is an invalid header"
+        
+        code_test_1 = "```python\nprint('hello world')\n```"
+        # invalid
+        code_test_2 = "```python\nprint('hello world')\nmissing closing backtics"
+
+        quote_test_1 = "> this is quoted\n> continuing the quote"
+        # invalid
+        quote_test_2 = "> this is quoted\nbreaking the quote with this line"
+
+        unordered_list_test_1 = "* line 1\n* line2\n* line 3\n* line 4"
+        # invalid
+        unordered_list_test_2 = "* line 1\n* line2\n line 3\n line 4"
+
+        ordered_list_test_1 = "1. line 1\n2. line two\n3. line three"
+        # invalid
+        ordered_list_test_2 = "1. line 1\n line two\n3. line three"
+        
+        # First test all valid cases
+        #
+        # Headers
+        self.assertEqual(block_to_block_type(header_test_1), BlockTypes.HEADING)
+        self.assertEqual(block_to_block_type(header_test_2), BlockTypes.HEADING)
+        self.assertEqual(block_to_block_type(header_test_3), BlockTypes.HEADING)
+        self.assertEqual(block_to_block_type(header_test_4), BlockTypes.HEADING)
+        self.assertEqual(block_to_block_type(header_test_5), BlockTypes.HEADING)
+        self.assertEqual(block_to_block_type(header_test_6), BlockTypes.HEADING)
+        # Code blocks
+        self.assertEqual(block_to_block_type(code_test_1), BlockTypes.CODE)
+        # Quote blocks
+        self.assertEqual(block_to_block_type(quote_test_1), BlockTypes.QUOTE)
+        # Unordered list blocks
+        self.assertEqual(block_to_block_type(unordered_list_test_1), BlockTypes.UNORDERED_LIST)
+        # Ordered list blocks 
+        self.assertEqual(block_to_block_type(ordered_list_test_1), BlockTypes.ORDERED_LIST)
+
+        # Check to see if invalid cases default to BlockTypes.PARAGRAPH
+        self.assertEqual(block_to_block_type(header_test_7), BlockTypes.PARAGRAPH)
+        self.assertEqual(block_to_block_type(header_test_8), BlockTypes.PARAGRAPH)
+        self.assertEqual(block_to_block_type(code_test_2), BlockTypes.PARAGRAPH)
+        self.assertEqual(block_to_block_type(quote_test_2), BlockTypes.PARAGRAPH)
+        self.assertEqual(block_to_block_type(unordered_list_test_2), BlockTypes.PARAGRAPH)
+        self.assertEqual(block_to_block_type(ordered_list_test_2), BlockTypes.PARAGRAPH)
+
+    def test_header_block_to_html(self):
+        header_test_1 = "# This is a h1 header"
+        header_test_2 = "## This is a h2 header"
+        header_test_3 = "### This is a h3 header"
+        header_test_4 = "#### This is a h4 header"
+        header_test_5 = "##### This is a h5 header"
+        header_test_6 = "###### This is a h6 header"
+        header_test_7 = "######This is an invalid header"
+        header_test_8 = "####### This is an invalid header"
+        header_test_9 = "This is an invalid header"
+
+        code_test_1 = "```python\nprint('hello world')\n```"
+        expected_code_test_1_output = ParentNode("pre", ParentNode("code", [
+            TextNode("print('hello world')", TextTypes.TEXT)
+        ]))
+        # invalid
+        code_test_2 = "```python\nprint('hello world')\nmissing closing backtics"
+
+        # Assert valid cases
+        self.assertEqual(header_block_to_html(header_test_1), LeafNode("h1", "This is a h1 header"))
+        self.assertEqual(header_block_to_html(header_test_2), LeafNode("h2", "This is a h2 header"))
+        self.assertEqual(header_block_to_html(header_test_3), LeafNode("h3", "This is a h3 header"))
+        self.assertEqual(header_block_to_html(header_test_4), LeafNode("h4", "This is a h4 header"))
+        self.assertEqual(header_block_to_html(header_test_5), LeafNode("h5", "This is a h5 header"))
+        self.assertEqual(header_block_to_html(header_test_6), LeafNode("h6", "This is a h6 header"))
+
+        # Assert invalid cases
+        for test in [header_test_7, header_test_8, header_test_9]:
+            try:
+                header_block_to_html(test)
+            except ValueError as e:
+                self.assertEqual(str(e), "header_block does not contain a valid markdown header")
+
+        self.assertEqual(code_block_to_html(code_test_1), expected_code_test_1_output)
